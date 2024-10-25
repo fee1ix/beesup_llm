59d08845ddfdf1361abf909ab7172c366aca0a46
fee1ix <felix.boehning@tu-ilmenau.de>
2024-10-25 13:20:33 +0000

    Refactor BaseTest class in inference/__init__.py

--- inference/__init__.py
+++ inference/__init__.py
@@ -1,14 +1,23 @@
 from beesup_llm import *
 from ..toolkit.setup_utils import *
 
+from ..dataset import BaseDataset
+
 import logging
 
 class BaseTest(BaseDirectory):
 
-    def __init__(self, config=None, parent_lab=None,):
+    def __init__(self, ref=None, dataset_ref=None, model_ref=None):
 
-        if config is None: #load default config from repository
-            current_dir = os.path.dirname(os.path.abspath(__file__))
-            config = os.path.join(current_dir, 'base_test_config.yaml')
-        
-        super().__init__(config, parent_lab)
+        self.type='test'
+        super().__init__(ref)
+
+        self._config_key_order.extend([])
+        self._config_keys_to_exclude.extend([])
+
+        self.dataset=BaseDataset(dataset_ref)
+        #self.model=BaseModel()
+
+
+
+
