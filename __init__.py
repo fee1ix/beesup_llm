ca8335781d86c5a229cef67288a62b1734fab1ed
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-05 09:19:55 +0000

    Refactor BaseDirectory class: Enhance initialization methods and streamline configuration handling; improve attribute updates and path management

--- __init__.py
+++ __init__.py
@@ -73,8 +73,10 @@ Total size of files: {total_size_mb:.2f} MB
 class BaseDirectory(object):
     """
     Base class for subdirectories inside a lab
+
+
     """
-    def __new__(cls, ref=None, skip_new=False, **kwargs):
+    def __new__supressed(cls, ref=None, skip_new=False, **kwargs):
 
         if skip_new:
             print('skip_new')
@@ -109,8 +111,7 @@ class BaseDirectory(object):
         
         return super().__new__(cls)
 
-    
-    def __init__(self, ref=None, **kwargs):
+    def __init__supressed(self, ref=None, **kwargs):
         self._config_key_order=['type', 'id', 'name', 'path', 'parent_dir_path', 'parent_lab_path']
         self._config_keys_to_exclude=['logger']
 
@@ -119,7 +120,9 @@ class BaseDirectory(object):
         self.logger = logging.getLogger(__name__)
         self.logger.info(f"{self.name.upper()} initialised")
 
-    def legacy__init__(self, ref=None):
+    def __init__(self, ref=None):
+
+        if not hasattr(self, 'type'): self.type='directory'
 
         self._config_key_order=['type', 'id', 'name', 'path', 'parent_dir_path', 'parent_lab_path']
         self._config_keys_to_exclude=['logger']
@@ -127,38 +130,10 @@ class BaseDirectory(object):
         if getattr(self, '_ref', None) is not None:
             ref=self._ref; del self._ref
         
-        if not hasattr(self, 'type'): self.type = 'directory'
-
-        if isinstance(ref, int): self.__init__from_id(ref)
-
-        elif isinstance(ref, str): self.__init__from_path(ref)
-
-        elif isinstance(ref, dict): self.__init__from_config(ref)
-
-        elif hasattr(ref, 'get_config'): self.__init__from_config(ref.get_config())
-
-        else: #initialize a new instance
-
-            self.parent_lab_path=extract_lab_path(os.getcwd())
-            self.parent_dir_path=f'{self.parent_lab_path}/{self.type}s' # derive the parent directory path from the type (e.g. dataset -> datasets)
-
-            if not os.path.exists(f'{self.parent_dir_path}'):
-                os.makedirs(f'{self.parent_dir_path}', exist_ok=False)
-
-            self.id=self.get_max_id()+1
-            self.name=f"{str(self.id).zfill(4)}_{self.type}"
-            self.path=f"{self.parent_dir_path}/{self.name}"
-
-            self.datetime_init=datetime.datetime.now(TIMEZONE)
-
-
+        self.__init__from_ref(ref)
+        
         self.logger = logging.getLogger(__name__)
         self.logger.info(f"{self.name.upper()} initialised")
-
-    def __init__from_config(self,config):
-        for k, v in config.items(): 
-            if (v is not None) and (not hasattr(self, k)):
-                setattr(self, k, v)
 
     def __init__from_id(self,id):
         assert hasattr(self, 'type'), "missing type key"
@@ -168,19 +143,61 @@ class BaseDirectory(object):
         
         assert os.path.exists(f'{self.parent_dir_path}'), f"parent directory {self.parent_dir_path} does not exist"
 
-        config = load_dict(f"{self.parent_dir_path}/{str(id).zfill(4)}_{self.type}/config.yaml")
-        self.__init__from_config(config)
+        config_dict = load_dict(f"{self.parent_dir_path}/{str(id).zfill(4)}_{self.type}/config.yaml")
+        self.__init__from_config_dict(config_dict)
 
-    def __init__from_path(self,path):
+    def __init__from_path(self, path):
 
         if not path.endswith('config.yaml'): 
             path=f"{path}/config.yaml"
 
         assert path.endswith('config.yaml'), "path must point to a 'config.yaml'"
-        config=load_dict(path)
+        config_dict = load_dict(path)
 
-        self.__init__from_config(config)
+        self.__init__from_config_dict(config_dict)
 
+    def __init__from_dict(self, the_dict):
+
+        if not is_valid_config(the_dict): raise ValueError("Invalid config dictionary")
+
+        self.__init__from_config_dict(the_dict)
+
+    def __init__from_config_dict(self,config_dict):
+
+        self.update_attributes(config_dict, overwrite=True)
+
+        # for k, v in config_dict.items(): 
+        #     if (v is not None) and (not hasattr(self, k)):
+        #         setattr(self, k, v)
+
+    def __init__from_none(self):
+
+        self.parent_lab_path = extract_lab_path(os.getcwd())
+        self.parent_dir_path = f'{self.parent_lab_path}/{self.type}s'  # derive the parent directory path from the type (e.g. dataset -> datasets)
+        if not os.path.exists(f'{self.parent_dir_path}'):
+            os.makedirs(f'{self.parent_dir_path}', exist_ok=False)
+
+        self.id = get_max_id(self.parent_dir_path) + 1
+        self.name = f"{str(self.id).zfill(4)}_{self.type}"
+        self.path = f"{self.parent_dir_path}/{self.name}"
+
+        self.datetime_init = get_datetime()
+    
+    def __init__from_ref(self, ref):
+
+        if not hasattr(self, 'type'): raise ValueError("type must be defined")
+
+        if isinstance(ref, int): self.__init__from_id(ref)
+
+        elif isinstance(ref, str): self.__init__from_path(ref)
+
+        elif isinstance(ref, dict): self.__init__from_dict(ref)
+        
+        elif hasattr(ref, 'get_config'): self.__init__from_config_dict(ref.get_config())
+
+        elif ref is None: self.__init__from_none()
+
+        
     def update_attributes(self, new_dict, overwrite=True):
         updated_config = update_nested_dict(self.get_config(), new_dict, overwrite)
 
@@ -190,15 +207,10 @@ class BaseDirectory(object):
         return
 
 
-    # def get_max_id(self):
-    #     assert hasattr(self, 'parent_dir_path'), "parent_dir_path must be defined"
+    def get_max_id(self):
+        assert hasattr(self, 'parent_dir_path'), "parent_dir_path must be defined"
+        return get_max_id(self.parent_dir_path)
 
-    #     max_id=0
-
-    #     id_list = get_ids(self.parent_dir_path)
-    #     if id_list: max_id = max(id_list)
-
-    #     return max_id
 
     def get_config(self):
 
@@ -218,9 +230,6 @@ class BaseDirectory(object):
 
         set_config(self.get_config())
         logging.info(f"{self.name.upper()} spawned at {self.path}")
-
-    def get_datetime(self):
-        return datetime.datetime.now(TIMEZONE)
 
     def __repr__(self):
 
