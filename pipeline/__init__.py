db112d2089ac140075ec890d6001a429e28bb881
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-06 14:13:00 +0000

    Add BasePipeline class: Implement initialization with dataset_ref support, enhance configuration handling, and integrate DataFrame compatibility

---
+++ pipeline/__init__.py
@@ -0,0 +1,29 @@
+from beesup_llm import *
+from ..toolkit.setup_utils import *
+from beesup_llm.dataset import BaseDataset
+
+import pandas as pd
+
+class BasePipeline(BaseDirectory):
+    type='pipeline'
+
+    def __init__(self, ref=None, dataset_ref=None, **kwargs):
+
+        super().__init__(ref, **kwargs)
+        self._config_key_order.extend([])
+        self._config_keys_to_exclude.extend(['df'])
+
+        self._default_config=dict()
+        self.update_attributes(self._default_config, overwrite=False)
+        
+        if dataset_ref is not None:
+            if isinstance(dataset_ref, pd.DataFrame):
+                self.df=dataset_ref
+
+            else:
+                dataset=BaseDataset(dataset_ref)
+                self.dataset_config=dataset.get_config()
+                self.df=dataset.dataset_df
+
+
+    
