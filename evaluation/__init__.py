3ea09b5b17ca6a04cefa440c5e9a50a2e8e5da8d
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-06 14:12:49 +0000

    Refactor BaseEvaluation class: Update initialization to accept dataset_ref, enhance configuration handling, and integrate DataFrame support

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -2,31 +2,33 @@ from beesup_llm import *
 from ..toolkit.setup_utils import *
 from ..toolkit.llm_utils import *
 
-#from beesup_llm.dataset import BaseDataset
+from beesup_llm.dataset import BaseDataset
 #from beesup_llm.model import BaseModelWrap
 import logging
 
+import pandas as pd
+
 
 class BaseEvaluation(BaseDirectory):
+    type='evaluation'
 
-    def __init__(self, ref=None, refs=[]):
+    def __init__(self, ref=None, dataset_ref=None, **kwargs):
 
-        self.type='evaluation'
-        super().__init__(ref)
+        super().__init__(ref,**kwargs)
 
         self._config_key_order.extend([])
-        self._config_keys_to_exclude.extend([])
+        self._config_keys_to_exclude.extend(['df'])
 
-        if hasattr(self, 'refs'):
-            refs = self.refs
-        
-        for i,ref in enumerate(refs):
-            if hasattr_or_key(ref, 'path'):
-                ref = getattr_or_key(ref, 'path')
-   
-            refs[i]=BaseDirectory(ref)
-        
-        self.refs=refs
+        self._default_config=dict()
+        self.update_attributes(self._default_config, overwrite=False)
+
+        if isinstance(dataset_ref, pd.DataFrame):
+            self.df=dataset_ref
+
+        else:
+            dataset=BaseDataset(dataset_ref)
+            self.dataset_config=dataset.get_config()
+            self.df=dataset.dataset_df
 
         self._default_config=dict()
         self._config_key_order.extend([k for k in self._default_config.keys() if k not in self._config_key_order])
