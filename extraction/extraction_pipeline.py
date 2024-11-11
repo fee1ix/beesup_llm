794cc29335cb84604ffea19bd2be9a2a4ebecdf0
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-11 14:50:59 +0000

    Refactor ExtractionPipeline: Simplify prompt handling and remove unused methods

--- extraction/extraction_pipeline.py
+++ extraction/extraction_pipeline.py
@@ -27,38 +27,18 @@ class ExtractionPipeline(object):
             #self.gen_model_config=self.modelwrap.get_config()
             #self.tokenizer=self.modelwrap.get_inference_tokenizer()
     
-    def get_prompt_ids(self, report_passage, use_extraction_prompt=True, use_few_shots=True):
-
-        prompt_messages=get_prompt_messages(report_passage, use_extraction_prompt, use_few_shots)
-        prompt_ids=self.modelwrap.inference_tokenizer.apply_chat_template(prompt_messages,tokenize=True)
-
-        return prompt_ids
-
-
     def __call__(self, report_passage, **kwargs):
 
-        if not hasattr_or_key(self.modelwrap, 'model'): self.modelwrap.load_model()
-        if not hasattr_or_key(self.modelwrap, 'inference_tokenizer'): self.modelwrap.load_inference_tokenizer()
+        prompt_messages=get_prompt_messages(report_passage, use_extraction_prompt=True, use_few_shots=True)
+        outputs=self.modelwrap(prompt_messages,**kwargs)
 
-        prompt_ids=self.get_prompt_ids(report_passage, **kwargs)
+        # #prompt_ids=self.get_prompt_ids(report_passage, **kwargs)
 
-        outputs=self.modelwrap.inference_step({'input':prompt_ids})
+        # outputs=self.modelwrap.inference_step({'input':prompt_ids})
 
         return outputs
-
-
-
-
-
-
+    
     
 
-
-    
-    def load_modelwrap(self):
-        self.modelwrap.load_model()
-    
-    def get_modelwrap(self):
-        self.modelwrap
 
 
