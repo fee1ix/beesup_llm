8eeb8f9994b002664c75ddc0ad9e7a8cf7479583
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-14 15:16:20 +0000

    Refactor BaseDirectory initialization: Simplify constructor and enhance configuration handling

--- __init__.py
+++ __init__.py
@@ -81,133 +81,51 @@ class BaseDirectory(object):
     _config_keys_to_exclude=['logger','from_ref']
     logger = logging.getLogger(__name__)
 
-    def __new__supressed(cls, ref=None, skip_new=False, **kwargs):
-
-        if skip_new:
-            print('skip_new')
-            return super().__new__(cls)
-
-        if is_valid_config(ref):
-            print('valid config')
-            config = ref
-
-        else: 
-            print('not valid config')
-            if 'type' not in kwargs: kwargs['type']='directory'
-            config=get_config_from_ref(ref, **kwargs)
-
-        if (cls is BaseDirectory):
-
-            if config['type'] == 'dataset':
-                from beesup_llm.dataset import BaseDataset
-                return BaseDataset(config)
-            
-            elif config['type'] == 'training': 
-                from beesup_llm.training import BaseTraining
-                return BaseTraining(config)
-
-            elif config['type'] == 'test': 
-                from beesup_llm.inference import BaseTest
-                return BaseTest(config)
-            
-            elif config['type'] == 'model': 
-                from beesup_llm.model import BaseModelWrap
-                return BaseModelWrap(config)
-        
-        return super().__new__(cls)
-
-    def __init__supressed(self, ref=None, **kwargs):
-        self._config_key_order=['type', 'id', 'name', 'path', 'parent_dir_path', 'parent_lab_path']
-        self._config_keys_to_exclude=['logger']
-
-        if is_valid_config(ref): self.update_attributes(ref,overwrite=True)
-
-        self.logger = logging.getLogger(__name__)
-        self.logger.info(f"{self.name.upper()} initialised")
-
-
-    def __init__legacy(self, ref=None, **kwargs):
-
-        if getattr(self, '_ref', None) is not None:
-            ref=self._ref; del self._ref
-        
-        self.__init__from_ref(ref)
-        
-        self.logger.info(f"{self.name.upper()} initialised")
-
-    # def __init__from_id(self,id):
-    #     assert hasattr(self, 'type'), "missing type key"
-
-    #     self.parent_lab_path=extract_lab_path(os.getcwd())
-    #     self.parent_dir_path=f'{self.parent_lab_path}/{self.type}s' # derive the parent directory path from the type (e.g. dataset -> datasets)
-        
-    #     assert os.path.exists(f'{self.parent_dir_path}'), f"parent directory {self.parent_dir_path} does not exist"
-
-    #     config_dict = load_dict(f"{self.parent_dir_path}/{str(id).zfill(4)}_{self.type}/config.yaml")
-    #     self.__init__from_config_dict(config_dict)
-
-    # def __init__from_path(self, path):
-
-    #     if not path.endswith('config.yaml'): 
-    #         path=f"{path}/config.yaml"
-
-    #     assert path.endswith('config.yaml'), "path must point to a 'config.yaml'"
-    #     config_dict = load_dict(path)
-
-    #     self.__init__from_config_dict(config_dict)
-
-    # def __init__from_dict(self, the_dict):
-
-    #     if not is_valid_config(the_dict): raise ValueError("Invalid config dictionary")
-
-    #     self.__init__from_config_dict(the_dict)
-
-    # def __init__from_config_dict(self,config_dict):
-
-    #     self.update_attributes(config_dict, overwrite=True)
-
-    #     # for k, v in config_dict.items(): 
-    #     #     if (v is not None) and (not hasattr(self, k)):
-    #     #         setattr(self, k, v)
-
-    # def __init__from_none(self):
-
-    #     self.parent_lab_path = extract_lab_path(os.getcwd())
-    #     self.parent_dir_path = f'{self.parent_lab_path}/{self.type}s'  # derive the parent directory path from the type (e.g. dataset -> datasets)
-    #     if not os.path.exists(f'{self.parent_dir_path}'):
-    #         os.makedirs(f'{self.parent_dir_path}', exist_ok=False)
-
-    #     self.id = get_max_id(self.parent_dir_path) + 1
-    #     self.name = f"{str(self.id).zfill(4)}_{self.type}"
-    #     self.path = f"{self.parent_dir_path}/{self.name}"
-
-    #     self.datetime_init = get_datetime()
-    
-    # def __init__from_ref(self, ref):
-
-    #     if not hasattr(self, 'type'): raise ValueError("type must be defined")
-
-    #     if isinstance(ref, int): self.__init__from_id(ref)
-
-    #     elif isinstance(ref, str): self.__init__from_path(ref)
-
-    #     elif isinstance(ref, dict): self.__init__from_dict(ref)
-        
-    #     elif hasattr(ref, 'get_config'): self.__init__from_config_dict(ref.get_config())
-
-    #     #elif ref is None: self.__init__from_none()
-
-    #     else: self.__init__from_none()
-
-
     def __init__(self, ref=None, **kwargs):
 
         kwargs.update(self.get_config())
         self.logger.debug(f"{self.__class__} ref={ref}, kwargs = {kwargs}\n")
 
         config_dict=get_config_from_ref(ref,**kwargs)
+
         self.update_attributes(config_dict, overwrite=True)
         self.logger.info(f"{self.name.upper()} initialised")
+
+    def reinit_config(self):
+
+        new_config=get_config_from_none(type=self.type)
+        self.update_attributes(new_config, overwrite=True)
+
+
+
+    def spawn_config(self):
+
+
+        if not os.path.exists(f'{self.parent_dir_path}'):
+            os.makedirs(f'{self.parent_dir_path}', exist_ok=False)
+
+        old_config=None
+        if os.path.exists(f'{self.path}/config.yaml'):
+            old_config = load_dict(f'{self.path}/config.yaml')
+
+        if old_config != self.get_config():
+
+
+
+
+
+
+
+
+
+        
+        
+
+            with open(f'{self.path}/config.yaml', 'r') as file:
+                config = yaml.safe_load(file)
+                self.update_attributes(config, overwrite=True)
+
+
 
 
     def update_attributes(self, new_dict, overwrite=True):
