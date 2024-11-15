ee15bb769243660b0cc4750b77219bc0115ec833
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-15 15:05:35 +0000

    Remove BaseExperiment class: Eliminate unused experiment class and related initialization code

--- experiment/__init__.py
+++ experiment/__init__.py
@@ -1,26 +1,3 @@
-from beesup_llm import *
-from ..toolkit.setup_utils import *
-from ..toolkit.llm_utils import *
 
 
-class BaseExperiment(BaseDirectory):
-    type='experiment'
 
-    def __init__(self, ref=None, dataset_ref=None, model_ref=None):
-
-
-        super().__init__(ref)
-
-        self._config_key_order.extend([])
-        self._config_keys_to_exclude.extend(['dataset_df'])
-
-        self._default_config=dict(
-            done = False,
-            batch_size = 4,
-            use_dataset_splits=['test'],
-            generation_config=dict(
-                do_sample=False,
-            ),
-        )
-
-        self.update_attributes(self._default_config, overwrite=False)
