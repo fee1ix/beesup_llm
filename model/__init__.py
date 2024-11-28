e878a5dc249655d339bc9dbeea26c52e1dccff85
fee1ix <felix.boehning@tu-ilmenau.de>
2024-11-28 16:14:02 +0000

    Refactor model initialization: Update configuration handling methods for improved clarity and prevent overwriting conflicts.

--- model/__init__.py
+++ model/__init__.py
@@ -59,7 +59,7 @@ class BaseModelWrap(BaseDirectory):
         self._config_keys_to_exclude.extend(['model'])#
 
         self._default_config=dict()
-        self.update_attributes(self._default_config, overwrite=False)
+        self.update_config(self._default_config, overwrite_if_conflict=False)
     
     def get_model(self):
 
@@ -77,6 +77,7 @@ class BaseModelWrap(BaseDirectory):
 from threading import Thread
 from transformers import TextGenerationPipeline, TextIteratorStreamer
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, GenerationConfig, set_seed
+
 class GenModelWrap(BaseModelWrap):
 
     type='gen_model'
@@ -97,9 +98,13 @@ class GenModelWrap(BaseModelWrap):
 
         pre_config = get_config_from_ref(ref, **kwargs)
 
-        if hasattr(ref,'model'): kwargs['model']=ref.model
-        if hasattr(ref,'inference_tokenizer'): kwargs['inference_tokenizer']=ref.inference_tokenizer
-        if hasattr(ref,'training_tokenizer'): kwargs['training_tokenizer']=ref.training_tokenizer
+        if getattr(ref, 'type', None) == cls.type:
+            if hasattr(ref,'model'): kwargs['model']=ref.model
+            if hasattr(ref,'inference_tokenizer'): kwargs['inference_tokenizer']=ref.inference_tokenizer
+            if hasattr(ref,'training_tokenizer'): kwargs['training_tokenizer']=ref.training_tokenizer
+        
+        if isinstance(ref, torch.nn.Module):
+            kwargs['model']=ref
 
         # if PeftLlamaModelWrap.matches(pre_config):
         #     return PeftLlamaModelWrap.from_ref(ref=pre_config, **kwargs)
@@ -146,7 +151,7 @@ class GenModelWrap(BaseModelWrap):
             )
         )
         
-        self.update_attributes(self._default_config, overwrite=False)
+        self.update_config(self._default_config, overwrite_if_conflict=False)
 
     def prepare_training(self):
         
@@ -500,6 +505,8 @@ class GenModelWrap(BaseModelWrap):
 
 
     def load_model(self):
+
+
         self.logger.info(f"Loading model {self.name_or_path}")
         self.model=AutoModelForCausalLM.from_pretrained(
             self.name_or_path,
@@ -588,7 +595,7 @@ class LlamaModelWrap(GenModelWrap):
             )
         )
         
-        self.update_attributes(self._default_config, overwrite=False)
+        self.update_config(self._default_config, overwrite_if_conflict=False)
 
 from peft import AutoPeftModelForCausalLM
 class PeftLlamaModelWrap(LlamaModelWrap):
@@ -638,21 +645,6 @@ class EmbModelWrap(BaseModelWrap):
         return cls(ref=pre_config, **kwargs)
 
 
-    # def __new__(cls, ref=None, skip_new=False, **kwargs):
-    #     kwargs.update(get_cls_attrs(cls))
-    #     cls.logger.debug(f"{cls} ref={ref}, kwargs = {kwargs}\n")
-    #     if skip_new: return super().__new__(cls)
-    #     if cls is not EmbModelWrap: return super().__new__(cls)
-
-    #     pre_config=get_config_from_ref(ref,**kwargs)
-
-    #     if cls.is_JinaaiModelWrap(pre_config):
-    #         cls.logger.debug(f"it's a JinaaiModelWrap!")
-    #         return JinaaiModelWrap(pre_config,**kwargs)
-
-    #     return EmbModelWrap(pre_config, skip_new=True, **kwargs)
-
-
     def __init__(self, ref=None, **kwargs):
         self.logger.debug(f"{self.__class__} ref={ref}, kwargs = {kwargs}\n")
         super().__init__(ref,**kwargs)
@@ -661,28 +653,74 @@ class EmbModelWrap(BaseModelWrap):
         self._config_keys_to_exclude.extend([])
 
         self._default_config=dict(
-            trust_remote_code=True,
-            use_flash_attn=False,
-            task='separation',
+            bnb_config=dict(
+                load_in_4bit=True,
+                bnb_4bit_use_double_quant=True,
+                bnb_4bit_quant_type='nf4',
+            ),
+            model_load_config=dict(
+                trust_remote_code=True,
+                
+            ),
+            encode_config=dict(  
+            ),
+
         )
 
-        self.update_attributes(self._default_config, overwrite=False)
+        self.update_config(self._default_config, overwrite_if_conflict=False)
     
-    def load_model(self):
+    def load_model(self,**kwargs):
+
+        self.logger.info(f"Loading model {self.name_or_path}")
+
+        bnb_config=self.get_updated_config(kwargs, config_key='bnb_config')
+        model_load_config=self.get_updated_config(kwargs, config_key='model_load_config')
+
+        if bnb_config:
+            quantization_config=BitsAndBytesConfig(
+                bnb_4bit_compute_dtype=torch.bfloat16,
+                **bnb_config
+                )
+        else:
+            quantization_config=None
+
 
         self.model=AutoModel.from_pretrained(
             self.name_or_path,
-            trust_remote_code=self.trust_remote_code,
-            use_flash_attn=self.use_flash_attn,
-            ).to('cuda')
+            device_map="auto",
+            quantization_config=quantization_config,
+
+            **model_load_config,
+        )
+
+        # self.model=AutoModel.from_pretrained(
+        #     self.name_or_path,
+        #     trust_remote_code=self.trust_remote_code,
+        #     use_flash_attn=self.use_flash_attn,
+        #     ).to('cuda')
+
+        return
+
+    # Add batching to your encode process
+    def batch_encode(self, chunks, batch_size=128, **kwargs):
+        embs = []
+        for i in range(0, len(chunks), batch_size):
+            batch = chunks[i:i + batch_size]
+            embs.append(self.model.encode(batch, **kwargs))
+            torch.cuda.empty_cache()
+
+        embs=torch.cat(embs, dim=0)
+        embs=embs.to('cpu')
+        return list(embs.numpy())
         
     def unique_encode(self, chunks, **kwargs):
 
         # Step 1: Remove redundancy by creating a set of unique chunks
         unique_chunks = list(set(chunks))
-        
+
         # Step 2: Encode each unique chunk
-        unique_embs = self.encode(unique_chunks, **kwargs)
+        unique_embs=self.batch_encode(unique_chunks, **kwargs)
+        #unique_embs = self.encode(unique_chunks, **kwargs)
         
         # Step 3: Create a mapping from each unique chunk to its embedding
         emb_dict = dict(zip(unique_chunks, unique_embs))
@@ -692,18 +730,20 @@ class EmbModelWrap(BaseModelWrap):
 
         return embs
 
+
     def encode(self, chunks, **kwargs):
 
-        encode_kwargs = dict(
-            task=self.task,
-        )
-        encode_kwargs.update(kwargs)
-
+        encode_config=self.get_updated_config(kwargs, config_key='encode_config')
 
         embs=self.model.encode(
             chunks,
-            **encode_kwargs
+            **encode_config
         )
+
+        if hasattr(embs,'device'):
+            if embs.device.type=='cuda':
+                embs=embs.to('cpu')
+
 
         return embs
 
@@ -724,12 +764,72 @@ class JinaaiModelWrap(EmbModelWrap):
 
         self._default_config=dict(
             name_or_path='jinaai/jina-embeddings-v3',
+            bnb_config=None,
+            model_load_config=dict(
+                use_flash_attn=False,
+                task='separation',
+            ),
+            encode_config=dict(
+            )
         )
-        self.update_attributes(self._default_config, overwrite=False)
+        self.update_config(self._default_config, overwrite_if_conflict=False)
 
 
+class NvidiaModelWrap(EmbModelWrap):
 
+    @staticmethod
+    def matches(ref):
+        if getattr_or_key(ref, 'name_or_path') == 'nvidia/NV-Embed-v2': return True
+        return False
+    
 
+    def __init__(self, ref=None, **kwargs):
+        self.logger.debug(f"{self.__class__} ref={ref}, kwargs = {kwargs}\n")
+        super().__init__(ref, **kwargs)
+
+        self._config_key_order.extend([])
+        self._config_keys_to_exclude.extend([])
+
+        self._default_config=dict(
+            name_or_path='nvidia/NV-Embed-v2',
+        
+            model_load_config=dict(
+            ),
+
+            encode_config=dict(
+                max_length = 32768,
+                instruction="Instruct: Retrieve the a suitable header for the chunk.\nChunk: ",
+            ),
+        )
+        self.update_config(self._default_config, overwrite_if_conflict=False)
+
+    # def load_model(self):
+
+    #     self.logger.info(f"Loading model {self.name_or_path}")
+    #     self.model=AutoModelForCausalLM.from_pretrained(
+    #         self.name_or_path,
+    #         device_map="auto",
+    #         quantization_config=BitsAndBytesConfig(
+    #             bnb_4bit_compute_dtype=torch.bfloat16,
+    #             **self.bnb_config
+    #             ),
+    #     )
+
+    #     return
+    
+    # def encode(self, chunks, **kwargs):
+
+    #     encode_kwargs = dict(
+    #         instruction=self.instruction,
+    #     )
+    #     encode_kwargs.update(kwargs)
+
+    #     embs=self.model.encode(
+    #         chunks,
+    #         **encode_kwargs
+    #     )
+
+    #     return embs
 
 
 
