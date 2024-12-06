999c450432e0d0c9bc67aed9fa5de27b79367ea1
febo2591 <felix.boehning@tu-ilmenau.de>
2024-12-06 17:06:17 +0000

    Refactor PredictionCallback: Rename methods for clarity and improve data saving functionality.

--- experiment/extraction_experiment.py
+++ experiment/extraction_experiment.py
@@ -16,23 +16,25 @@ from transformers import TrainerCallback
 class PredictionCallback(TrainerCallback):
     def __init__(self, experiment):
         self.experiment = experiment
+        self.name='pred_callback'
         pass
 
-    def get_eval_df(self, model):
-        eval_df=self.experiment.eval_df
-        eval_df=self.experiment.extractor_pipe(eval_df, llm_ref=model)
-        return eval_df
+    def get_pred_df(self, model):
+        pred_df=self.experiment.eval_df
+        pred_df=self.experiment.extractor_pipe(pred_df, llm_ref=model)
+        print(f"_recent_generation_config: {self.experiment.extractor_pipe.llm_pipe._recent_generation_config}")
+        return pred_df
     
-    def save_eval_df(self, eval_df, global_step):
-        eval_df.to_pickle(f"{self.experiment._path}/{str(global_step).zfill(4)}_eval_df.pkl")
-        self.experiment.logger.info(f"Saved eval_df to {self.experiment._path}/{str(global_step).zfill(4)}_eval_df.pkl")
+    def save_df(self, df, global_step):
+        save_path=f"{self.experiment._path}/{str(global_step).zfill(4)}_{self.name}_df.pkl"
+        df.to_pickle(save_path)
+        self.experiment.logger.info(f"Saved {self.name} to {save_path}")
 
     def on_epoch_end(self, args, state, control, **kwargs):
         model=kwargs['model']
         model.eval()
-        eval_df=self.get_eval_df(model)
-        self.save_eval_df(eval_df,state.global_step)
-        
+        pred_df=self.get_pred_df(model)
+        self.save_df(pred_df, state.global_step)
 
 class ExtractionExperiment(BaseDirectory):
     type='extraction_experiment'
@@ -52,6 +54,8 @@ class ExtractionExperiment(BaseDirectory):
             overview_df=cls.get_overview(keypaths=['done'])
             multirun_df=overview_df[overview_df['done']==False].copy()
             multirun_df.reset_index(drop=True, inplace=True)
+
+        
         
 
         multirun_config=dict(
@@ -70,22 +74,22 @@ class ExtractionExperiment(BaseDirectory):
         print()
 
         return multirun_df
+    
 
-    def __init__(self, ref=None, dataset_ref=None, extractor_ref=None, llm_ref=None, trainer_ref=None, **kwargs):
+    def __init__(self, ref=None, dataset_ref=None, extractor_ref=None, llm_ref=None, trainer_ref=None, **kwargs): 
         super().__init__(ref, **kwargs)
 
         self._default_config=dict(
             done = False,
             seed = 55,
             do_eval_base_model=True,
+            do_train=True,
 
             llm_config=dict(
-                #name_or_path='meta-llama/Meta-Llama-3.1-8B-Instruct',
                 generation_config=dict(
                     do_sample=False,
-                ),
-                dataloader_config=dict(
-                    batch_size=10,
+                    max_new_tokens=1000,
+                    max_time=600,
                 ),
             ),
 
@@ -103,52 +107,53 @@ class ExtractionExperiment(BaseDirectory):
         self._config_key_order.extend(list(self._default_config.keys()))
         self._config_keys_to_exclude.extend(['dataset_df','train_df','test_df','eval_df','train_ds','eval_ds','trainer','trainwrap','llm_pipe','extractor_pipe','dataset'])
 
-
-        self.logger.debug(f"ref: {ref}")
-        self.logger.debug(f"kwargs: {kwargs}")
-
         self.update_config(self._default_config, overwrite_if_conflict=False)
+        self.update_config_smart(kwargs)
 
-        self.update_config_smart(
-            kwargs, 
-            interpret_none_as_val=True, 
-            overwrite_if_conflict=True, 
-            allow_new_atomic_keys=False, 
-            allow_new_nested_keys=False
-        )
-
-        self.trainer_config['trainer_args']['seed']=self.seed
-
-        llm_ref = llm_ref or getattr(self, 'llm_config', None)
-        if llm_ref is not None:
-            self.logger.debug(f"llm_ref: {llm_ref}")
-            self.llm_pipe=LanguageModelPipeline.from_ref(llm_ref)
-            self.update_config(dict(llm_config=self.llm_pipe.get_config()), overwrite_if_conflict=False)
+        if self.is_spawned():
+            llm_ref=self.llm_config
+            dataset_ref=self.dataset_config
+            trainer_ref=self.trainer_config
+            extractor_ref=self.extractor_config
 
 
-        dataset_ref = dataset_ref or getattr(self, 'dataset_config', None)
-        if dataset_ref is not None:
-            self.dataset=BaseDataset.from_ref(dataset_ref)
-            self.update_config(dict(dataset_config=self.dataset.get_config()), overwrite_if_conflict=False)
+        self.llm_pipe=LanguageModelPipeline.from_ref(llm_ref)
+        #self.llm_pipe.update_config(self.llm_config)
+        self.llm_pipe.update_config(self._default_config['llm_config'])
+        self.llm_pipe.update_config_smart(kwargs)
+        #self.update_config(dict(llm_config=self.llm_pipe.get_config()), overwrite_if_conflict=False)
+        self.llm_config=self.llm_pipe.get_config()
         
 
-        trainer_ref = trainer_ref or getattr(self, 'trainer_config', None)
-        if trainer_ref is not None:
-            self.trainwrap=BaseTrainerWrap.from_ref(trainer_ref)
-            self.update_config(dict(trainer_config=self.trainwrap.get_config()), overwrite_if_conflict=False)
+        self.dataset=BaseDataset.from_ref(dataset_ref)
+        #self.update_config(dict(dataset_config=self.dataset.get_config()), overwrite_if_conflict=False)
+        self.dataset_config=self.dataset.get_config()
 
-        extractor_ref = extractor_ref or getattr(self, 'extractor_config', None)
-        if extractor_ref is not None:
-            self.extractor_pipe=ExtractionPipeline.from_ref(extractor_ref)
-            self.update_config(dict(extractor_pipe_config=self.extractor_pipe.get_config()), overwrite_if_conflict=False)
+
+        self.trainwrap=BaseTrainerWrap.from_ref(trainer_ref)
+        #self.trainwrap.update_config(self.trainer_config)
+        self.trainwrap.update_config(self._default_config['trainer_config'])
+        self.trainwrap.update_config_smart(kwargs)
+        self.trainer_config['trainer_args']['seed']=self.seed
+        #self.update_config(dict(trainer_config=self.trainwrap.get_config()), overwrite_if_conflict=False)
+        self.trainer_config=self.trainwrap.get_config()
+
+
+        self.extractor_pipe=ExtractionPipeline.from_ref(extractor_ref, llm_ref=self.llm_pipe)
+        self.extractor_pipe.update_config_smart(kwargs)
+        #self.update_config(dict(extractor_config=self.extractor_pipe.get_config()), overwrite_if_conflict=False)
+        self.extractor_config=self.extractor_pipe.get_config()
+        
+
+        
 
     def load_data(self, **kwargs):
         self.logger.info(f"Loading Data")
 
         assert hasattr(self, 'dataset'), "Dataset must be assigned before loading data"
-        assert hasattr(self, 'extractor_pipe'), "Extraction extractor_pipe must be assigned before loading data"
         assert hasattr(self, 'llm_pipe'), "llm_pipe must be assigned before loading data"
-
+        assert hasattr(self, 'extractor_pipe'), "Extraction extractor_pipe must be assigned before loading data"
+        
         train_df=self.dataset.get_df_splits('train')
         eval_df=self.dataset.get_df_splits(['test','eval'])
 
@@ -172,9 +177,6 @@ class ExtractionExperiment(BaseDirectory):
 
         self.logger.info(f"Loading Trainer")
 
-        # trainer_args=self.get_updated_config(kwargs, 'trainer_args')
-        # self.logger.debug(f"trainer_args: {trainer_args}")
-
         trainer= self.trainwrap.get_trainer(
             model=model,
             tokenizer=self.llm_pipe.get_training_tokenizer(),
@@ -183,15 +185,13 @@ class ExtractionExperiment(BaseDirectory):
         )
 
         trainer.add_callback(PredictionCallback(self))
-
         return trainer
     
     def evaluate_base_model(self,model):
 
-        eval_callback=PredictionCallback(self)
-        
-        eval_df=eval_callback.get_eval_df(model)
-        eval_callback.save_eval_df(eval_df,0)
+        pred_callback=PredictionCallback(self)
+        pred_df=pred_callback.get_pred_df(model)
+        pred_callback.save_df(pred_df,0)
 
     def run(self,**kwargs):
 
@@ -209,16 +209,16 @@ class ExtractionExperiment(BaseDirectory):
 
         if self.do_eval_base_model:
             self.evaluate_base_model(model=base_model)
-
-        trainer=self.get_trainer(model=base_model,**kwargs)
-        trainer_args=trainer.args.to_dict()
-        save_yaml(trainer_args, f"{self._path}/trainer_args.yaml")
-
-        trainer.train()
+        
+        if self.do_train:
+            trainer=self.get_trainer(model=base_model,**kwargs)
+            trainer_args=trainer.args.to_dict()
+            save_yaml(trainer_args, f"{self._path}/trainer_args.yaml")
+            trainer.train()
 
         self.done=True
         self.timestamp_done=get_timestamp()
-        set_config(self.get_config())
+        set_config(self.get_config(),path=self._path)
         return
     
     def spawn(self):
@@ -227,22 +227,21 @@ class ExtractionExperiment(BaseDirectory):
         if not self.dataset.is_spawned(): self.dataset.spawn()
         if not self.trainwrap.is_spawned(): self.trainwrap.spawn()
         if not self.extractor_pipe.is_spawned(): self.extractor_pipe.spawn()
-        
         super().spawn()
 
-    def get_evals_df(self):
+    def get_callbacks_df(self):
 
         fns=os.listdir(self._path)
-        fns=[fn for fn in fns if fn.endswith('eval_df.pkl')]
-        evals_df=pd.DataFrame()
+        fns=[fn for fn in fns if fn.endswith('df.pkl')]
+        callbacks_df=pd.DataFrame()
         for fn in fns:
             global_step=int(fn[:4])
-            eval_df=pd.read_pickle(f"{self._path}/{fn}")
-            eval_df['global_step']=global_step
-            evals_df=pd.concat([evals_df,eval_df])
+            callback_df=pd.read_pickle(f"{self._path}/{fn}")
+            callback_df['global_step']=global_step
+            callbacks_df=pd.concat([callbacks_df,callback_df])
 
-        evals_df.reset_index(drop=True, inplace=True)
-        return evals_df
+        callbacks_df.reset_index(drop=True, inplace=True)
+        return callbacks_df
 
 
 
